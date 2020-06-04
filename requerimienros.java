c18f745d718377103d848265cb44dd74e15b0d2c
STIVEN-J4 <65369672+STIVEN-J4@users.noreply.github.com>
2020-06-04 15:29:55 -0500

    requerimientos

---
+++ requerimienros.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author STIVEN
+ */
+import java.util.Scanner;
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+         Scanner objeto=new Scanner (System.in);
+
+    System.out.println("\nObjetos del mundo real:");
+    String real=objeto.nextLine();
+
+
+    System.out.println("\nObjetos del mundo informatico");
+
+    System.out.println("\nObjeto ");
+    String objeto1=objeto.nextLine();
+
+    System.out.println("\nAtributos");
+
+    System.out.println("Atributo 1:");
+    String atributo1=objeto.nextLine();
+
+    System.out.println(" Atributo 2:");
+    String atributo2=objeto.nextLine();
+
+    System.out.println(" Atributo 3:");
+    String atributo3=objeto.nextLine();
+
+    System.out.println("\nServicios");
+
+    System.out.println("Servicio 1:");
+    String servicio1=objeto.nextLine();
+
+    System.out.println(" Servicio 2:");
+    String servicio2=objeto.nextLine();
+
+    System.out.println(" Servicio 3:");
+    String servicio3=objeto.nextLine();
+
+    }
+
+}
